0fe4fa89b5f6b50a6779f58b6774059a0ec7a30c
philipfrerk <philip.frerk@icloud.com>
2020-10-06 20:51:39 +0200

    .

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { catchError } from 'rxjs/operators'; 
+import { Observable, of } from 'rxjs';
+
 
 @Component({
   selector: 'app-contact',
@@ -13,7 +16,8 @@ export class ContactComponent implements OnInit {
   @ViewChild('inputName', { static: true }) inputName: any;
   @ViewChild('inputMailText', { static: true }) inputMailText: any;
 
-  messageSent = false;
+  messageSentSuccess = false;
+  messageSentFailure = false;
 
   constructor(private httpClient: HttpClient) { }
 
@@ -22,7 +26,7 @@ export class ContactComponent implements OnInit {
 
   btnSendDisabled(): boolean {
     const hasMail = this.inputMailFrom.nativeElement.value && this.inputMailFrom.nativeElement.value.toString().trim()
-                    && this.inputMailFrom.nativeElement.value.toString().indexOf('@')!==-1;
+      && this.inputMailFrom.nativeElement.value.toString().indexOf('@') !== -1;
     const hasName = this.inputName.nativeElement.value && this.inputName.nativeElement.value.trim();
     const hasText = this.inputMailText.nativeElement.value && this.inputMailText.nativeElement.value.trim();
     return !hasMail || !hasName || !hasText;
@@ -31,16 +35,32 @@ export class ContactComponent implements OnInit {
   sendMail() {
     const url = `http://${environment.backendServerAdress}:${environment.backendServerPort}/sendmail/
                 ${this.inputMailFrom.nativeElement.value}/
-                ${this.inputName.nativeElement.value }/
-                ${this.inputMailText.nativeElement.value }`;
+                ${this.inputName.nativeElement.value}/
+                ${this.inputMailText.nativeElement.value}`;
+    
 
-    this.httpClient.post(url, null).subscribe(() => {});
+    this.httpClient.post(url, null)
+      .pipe(
+        catchError( () =>this.handleError())
+      )
+      .subscribe((response: any) => {
+        console.log(response)
+        if (response.statusCode === 200) {
+          this.messageSentSuccess = true;
+        } else {
+          this.messageSentFailure = true;
+        }
+      });
 
-    this.messageSent = true;
 
     this.inputMailFrom.nativeElement.value = '';
     this.inputMailText.nativeElement.value = '';
     this.inputName.nativeElement.value = '';
+  }
+
+  private handleError(): Observable<any> {
+    this.messageSentFailure = true;
+    return of({});
   }
 
 }
